a7c6f44588b78e348e89733dd662749b90cce025
khadija_hussein <142981881+khadijahusseinali@users.noreply.github.com>
2023-09-28 18:00:33 +0300

    added

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 70,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+        
+    }
+})
